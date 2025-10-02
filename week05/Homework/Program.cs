89b93c3cd3fa4e650617b4b0d4a2cfcc8d9b50fd
Kelsey-Hansel <kelseylhansel@gmail.com>
2025-10-01 22:34:38 -0500

    Learning Activity assignment coding.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment studentAssignment = new Assignment("James Bond", "Spying");
+        Console.WriteLine(studentAssignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment studentMath = new MathAssignment("Josh Boyd", "Algebra", "5.2", "6-23");
+        Console.WriteLine(studentMath.GetSummary());
+        Console.WriteLine(studentMath.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment studentWriting = new WritingAssignment("Amanda Brown", "English III", "The Benefits of Studying to Music");
+        Console.WriteLine(studentWriting.GetSummary());
+        Console.WriteLine(studentWriting.GetWritingInformation());
+        Console.WriteLine();
     }
 }
